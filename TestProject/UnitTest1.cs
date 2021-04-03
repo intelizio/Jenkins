8811159a54f15afbe68a96415b118ae24df203bc
intelizio <rosiles@gmail.com>
2021-04-03 17:06:13 -0600

    asd

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //Assert.Pass();
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
